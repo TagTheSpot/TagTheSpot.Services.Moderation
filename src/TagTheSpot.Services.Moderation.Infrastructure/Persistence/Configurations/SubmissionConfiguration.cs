3a8f91d35f04b96eb9905546f0deb4b9daa26163
Savchenko Artem <p.savchenkoad.back@gmail.com>
2025-09-20 10:27:46 +0300

    [TTS-183] Add Entity Level Configuration (#10)
    https://psavchenkoad.atlassian.net/browse/TTS-183?atlOrigin=eyJpIjoiZDFlZjJlZTZiZjE0NDhhYWE2YTZiNzJhZTZmOTRiNWQiLCJwIjoiaiJ9

---
+++ src/TagTheSpot.Services.Moderation.Infrastructure/Persistence/Configurations/SubmissionConfiguration.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using TagTheSpot.Services.Moderation.Domain.Submissions;
+using TagTheSpot.Services.Moderation.Domain.Users;
+
+namespace TagTheSpot.Services.Moderation.Infrastructure.Persistence.Configurations
+{
+    internal sealed class SubmissionConfiguration
+        : IEntityTypeConfiguration<Submission>
+    {
+        public void Configure(EntityTypeBuilder<Submission> builder)
+        {
+            builder.HasKey(sub => sub.Id);
+
+            builder.Property(sub => sub.CityName)
+                   .HasMaxLength(100)
+                   .IsRequired();
+
+            builder.Property(sub => sub.Description)
+                   .HasMaxLength(1000)
+                   .IsRequired();
+
+            builder.Property(s => s.SpotType)
+                   .HasConversion<string>()
+                   .IsRequired();
+
+            builder.Property(s => s.Status)
+                   .HasConversion<string>()
+                   .IsRequired();
+
+            builder.Property(s => s.SkillLevel)
+                   .HasConversion<string>();
+
+            builder.Property(s => s.Accessibility)
+                   .HasConversion<string>();
+
+            builder.Property(s => s.Condition)
+                   .HasConversion<string>();
+
+            builder.Property(s => s.RejectionReason)
+                   .HasMaxLength(500);
+
+            builder.HasOne<User>()
+                .WithMany()
+                .HasForeignKey(sub => sub.UserId)
+                .OnDelete(DeleteBehavior.Cascade);
+        }
+    }
+}
